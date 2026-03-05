be22611d6cb89db69354a588a315f66c4db51298
Eti <e6766817@gmail.com>
2026-03-05 16:42:36 +0200

    good migrtion for category

---
+++ server/WebApi/english_project_server/Migrations/20260305144027_AddCategoryColumnToLevelTestQuestions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace english_project_server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryColumnToLevelTestQuestions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "LevelTestQuestions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "LevelTestQuestions");
+        }
+    }
+}
