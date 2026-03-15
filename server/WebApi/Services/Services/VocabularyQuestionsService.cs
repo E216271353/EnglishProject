8f61311435fea6f84324bb904640438427fff15e
Elisheva Ganz <elisheva0504106555@gmail.com>
2026-03-15 16:24:39 +0200

    Vocabulary questions service

---
+++ server/WebApi/Services/Services/VocabularyQuestionsService.cs
@@ -0,0 +1,26 @@
+﻿using Repository.Entities;
+using Repository.Repositories;
+using Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services
+{
+        public class VocabularyQuestionsService : IVocabularyQuestions
+        {
+            private readonly VocabularyQuestionsRepository _repository;
+
+            public VocabularyQuestionsService(VocabularyQuestionsRepository repository)
+            {
+                _repository = repository;
+            }
+            public async Task<IEnumerable<VocabularyQuestions>> GetVocabularyQuestions()
+            {
+                return await _repository.GetAllAsync();
+            }
+        }
+    
+}
